f5222710be5a9e24025d1aca27ff1c42c84a39d9
Kovács András <kovi91@gmail.com>
2018-07-28 20:04:17 +0200

    #1: Portszámokhoz 5555 hozzáadva, localhost-on kívüli elérés biztosítva

--- LearnWords/Program.cs
+++ LearnWords/Program.cs
@@ -20,6 +20,7 @@ namespace LearnWords
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls(urls: "http://*:5555")
                 .Build();
     }
 }
